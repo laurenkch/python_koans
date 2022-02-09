dc34418066452126f0d1d3c9c4e857a922a7a34e
laurenkch <lkoch879@gmail.com>
2022-02-08 20:14:24 -0500

    methods

--- koans/about_methods.py
+++ koans/about_methods.py
@@ -7,12 +7,14 @@
 
 from runner.koan import *
 
-def my_global_function(a,b):
+
+def my_global_function(a, b):
     return a + b
+
 
 class AboutMethods(Koan):
     def test_calling_a_global_function(self):
-        self.assertEqual(__, my_global_function(2,3))
+        self.assertEqual(5, my_global_function(2, 3))
 
     # NOTE: Wrong number of arguments is not a SYNTAX error, but a
     # runtime error.
@@ -25,7 +27,7 @@ class AboutMethods(Koan):
         # Note, the text comparison works for Python 3.2
         # It has changed in the past and may change in the future
         self.assertRegex(msg,
-            r'my_global_function\(\) missing 2 required positional arguments')
+                         r'my_global_function\(\) missing 2 required positional arguments')
 
         try:
             my_global_function(1, 2, 3)
@@ -33,7 +35,11 @@ class AboutMethods(Koan):
             msg = e.args[0]
 
         # Note, watch out for parenthesis. They need slashes in front!
-        self.assertRegex(msg, __)
+        self.assertRegex(
+            msg, 
+            r'my_global_function\(\) takes 2 positional arguments but 3 were given')
+
+    #python will let you know you added too many or not enough arguments in the exception argument(message)
 
     # ------------------------------------------------------------------
 
@@ -41,7 +47,7 @@ class AboutMethods(Koan):
         sum = a + b
 
     def test_which_does_not_return_anything(self):
-        self.assertEqual(__, self.pointless_method(1, 2))
+        self.assertEqual(None, self.pointless_method(1, 2))
         # Notice that methods accessed from class scope do not require
         # you to pass the first "self" argument?
 
@@ -51,8 +57,10 @@ class AboutMethods(Koan):
         return [a, b]
 
     def test_calling_with_default_values(self):
-        self.assertEqual(__, self.method_with_defaults(1))
-        self.assertEqual(__, self.method_with_defaults(1, 2))
+        self.assertEqual([1,'default_value'], self.method_with_defaults(1))
+        self.assertEqual([1, 2], self.method_with_defaults(1, 2))
+
+        #other variables would get precident before the default, default will only get used if not enough arguments are passed in. 
 
     # ------------------------------------------------------------------
 
@@ -60,9 +68,12 @@ class AboutMethods(Koan):
         return args
 
     def test_calling_with_variable_arguments(self):
-        self.assertEqual(__, self.method_with_var_args())
+        self.assertEqual((), self.method_with_var_args())
+        #returns an emtpy tuple.
         self.assertEqual(('one',), self.method_with_var_args('one'))
-        self.assertEqual(__, self.method_with_var_args('one', 'two'))
+        self.assertEqual(('one','two'), self.method_with_var_args('one', 'two'))
+
+#
 
     # ------------------------------------------------------------------
 
@@ -73,13 +84,18 @@ class AboutMethods(Koan):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, function_with_the_same_name(3,4))
+        self.assertEqual(12, function_with_the_same_name(3, 4))
+   
+    #I thought initially this would be 7, but I guess because we don't use "self" when we call the function 
+
 
     def test_calling_methods_in_same_class_with_explicit_receiver(self):
         def function_with_the_same_name(a, b):
             return a * b
 
-        self.assertEqual(__, self.function_with_the_same_name(3,4))
+        self.assertEqual(7, self.function_with_the_same_name(3, 4))
+
+        # this one uses self, so I guess it is therefore linked to the first one where we also put "self" as one of the parameters.
 
     # ------------------------------------------------------------------
 
@@ -92,10 +108,12 @@ class AboutMethods(Koan):
         return 42
 
     def test_that_old_methods_are_hidden_by_redefinitions(self):
-        self.assertEqual(__, self.another_method_with_the_same_name())
+        self.assertEqual(42, self.another_method_with_the_same_name())
 
     def test_that_overlapped_method_is_still_there(self):
-        self.assertEqual(__, self.link_to_overlapped_method())
+        self.assertEqual(10, self.link_to_overlapped_method())
+
+        #returns 10 because link to overlapped method ran before the function was changed and is therefore still the old method.
 
     # ------------------------------------------------------------------
 
@@ -103,21 +121,27 @@ class AboutMethods(Koan):
         pass
 
     def test_methods_that_do_nothing_need_to_use_pass_as_a_filler(self):
-        self.assertEqual(__, self.empty_method())
+        self.assertEqual(None, self.empty_method())
 
     def test_pass_does_nothing_at_all(self):
         "You"
         "shall"
         "not"
         pass
-        self.assertEqual(____, "Still got to this line" != None)
+        self.assertEqual(True, "Still got to this line" != None)
+
+        #I guess the assertequal is true becuase the sting doesn't equal none. 
+
+        #More broadly, pass is used when you want to write a function but it doesn't have a purpose yet. It can't just be empty.  
+
 
     # ------------------------------------------------------------------
 
     def one_line_method(self): return 'Madagascar'
 
     def test_no_indentation_required_for_one_line_statement_bodies(self):
-        self.assertEqual(__, self.one_line_method())
+        self.assertEqual('Madagascar', self.one_line_method())
+
 
     # ------------------------------------------------------------------
 
@@ -126,8 +150,10 @@ class AboutMethods(Koan):
         return "ok"
 
     def test_the_documentation_can_be_viewed_with_the_doc_method(self):
-        self.assertRegex(self.method_with_documentation.__doc__, __)
+        self.assertRegex(self.method_with_documentation.__doc__,
+                         'A string placed at the beginning of a function is used for documentation')
 
+        #__doc__ returns the documentation, whereas if you just call the function regularly you'd get 'ok'
     # ------------------------------------------------------------------
 
     class Dog:
@@ -139,24 +165,29 @@ class AboutMethods(Koan):
             return "wagging"
 
         def __password(self):
-            return 'password' # Genius!
+            return 'password'  # Genius!
 
     def test_calling_methods_in_other_objects(self):
         rover = self.Dog()
-        self.assertEqual(__, rover.name())
+        self.assertEqual('Fido', rover.name())
 
     def test_private_access_is_implied_but_not_enforced(self):
         rover = self.Dog()
 
         # This is a little rude, but legal
-        self.assertEqual(__, rover._tail())
+        self.assertEqual("wagging", rover._tail())
+
+        #I suppose rude because the underscore implies this function won't be used on other objects, just Dog, but here we're calling it on another function anyways. 
 
     def test_attributes_with_double_underscore_prefixes_are_subject_to_name_mangling(self):
         rover = self.Dog()
-        with self.assertRaises(___): password = rover.__password()
+        with self.assertRaises(AttributeError):
+            password = rover.__password()
 
         # But this still is!
-        self.assertEqual(__, rover._Dog__password())
+        self.assertEqual("password", rover._Dog__password())
 
         # Name mangling exists to avoid name clash issues when subclassing.
         # It is not for providing effective access protection
+
+        #name mangling = the ._Dog part before __password which python seems to add in automatically. 
--- koans/about_tuples.py
+++ koans/about_tuples.py
@@ -6,7 +6,9 @@ from runner.koan import *
 class AboutTuples(Koan):
     def test_creating_a_tuple(self):
         count_of_three =  (1, 2, 5)
-        self.assertEqual(__, count_of_three[2])
+        self.assertEqual(5, count_of_three[2])
+
+        #returns the value at the index of 2
 
     def test_tuples_are_immutable_so_item_assignment_is_not_possible(self):
 
@@ -19,13 +21,18 @@ class AboutTuples(Koan):
         # Note, assertRegex() uses regular expression pattern matching,
         # so you don't have to copy the whole message.
 
-        self.assertRegex(msg, __)
+        self.assertRegex(msg, "'tuple' object does not support item assignment")
+
+        #you can't just replace one item in a tuple
 
     def test_tuples_are_immutable_so_appending_is_not_possible(self):
         count_of_three =  (1, 2, 5)
-        with self.assertRaises(___): count_of_three.append("boom")
+        with self.assertRaises(AttributeError):
+            count_of_three.append("boom")
 
         # Tuples are less flexible than lists, but faster.
+
+        #can't append a tuple, there is no such attribute for tuples. Can't look up the tree to see what to do. The assertRaises function above looks to see what kind of exception was thrown when you run the function below. 
 
     def test_tuples_can_only_be_changed_through_replacement(self):
         count_of_three = (1, 2, 5)
@@ -34,26 +41,36 @@ class AboutTuples(Koan):
         list_count.append("boom")
         count_of_three = tuple(list_count)
 
-        self.assertEqual(__, count_of_three)
+        self.assertEqual((1,2,5,"boom"), count_of_three)
+
+        #you can change a tuple if you replace the whole thing at once (or make a copy and then replace the whole thing if you're changing a portion)
 
     def test_tuples_of_one_look_peculiar(self):
-        self.assertEqual(__, (1).__class__)
-        self.assertEqual(__, (1,).__class__)
-        self.assertEqual(__, ("I'm a tuple",).__class__)
-        self.assertEqual(__, ("Not a tuple").__class__)
+        self.assertEqual(int, (1).__class__)
+        self.assertEqual(tuple, (1,).__class__)
+        self.assertEqual(tuple, ("I'm a tuple",).__class__)
+        self.assertEqual(str, ("Not a tuple").__class__)
+
+    #single tuples use parenthesis and a comma at the end. without the comma it is not a tuple. 
 
     def test_tuple_constructor_can_be_surprising(self):
-        self.assertEqual(__, tuple("Surprise!"))
+        self.assertEqual(("S","u","r","p","r","i","s","e","!",), tuple("Surprise!"))
+
+    #making a tuple from a single string will separate out the letters. 
 
     def test_creating_empty_tuples(self):
-        self.assertEqual(__ , ())
-        self.assertEqual(__ , tuple()) #Sometimes less confusing
+        self.assertEqual(() , ())
+        self.assertEqual(() , tuple()) #Sometimes less confusing
+
+    #you can make an empty tuple with empty parentheses
 
     def test_tuples_can_be_embedded(self):
         lat = (37, 14, 6, 'N')
         lon = (115, 48, 40, 'W')
         place = ('Area 51', lat, lon)
-        self.assertEqual(__, place)
+        self.assertEqual(('Area 51',(37, 14, 6, 'N'), (115, 48, 40, 'W')) , place)
+    
+    #tuples can nest and you can drill down in them to read values. 
 
     def test_tuples_are_good_for_representing_records(self):
         locations = [
@@ -63,5 +80,8 @@ class AboutTuples(Koan):
 
         locations.append( ("Cthulu", (26, 40, 1, 'N'), (70, 45, 7, 'W')) )
 
-        self.assertEqual(__, locations[2][0])
-        self.assertEqual(__, locations[0][1][2])
+        self.assertEqual("Cthulu", locations[2][0])
+        self.assertEqual(15.56, locations[0][1][2])
+
+        #didn't realize at first this was a list of tuples and I initially thought you wouldn't be able to add the "Cthulu" one to this list. 
+        
