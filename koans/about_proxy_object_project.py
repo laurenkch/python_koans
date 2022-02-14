7edf8c8229886a442c27f2ed746923a8a9dd3cf2
laurenkch <lkoch879@gmail.com>
2022-02-13 21:39:43 -0500

    update for proxy object

--- koans/about_proxy_object_project.py
+++ koans/about_proxy_object_project.py
@@ -22,27 +22,36 @@ class Proxy:
     def __init__(self, target_object):
         # WRITE CODE HERE
         # self._call_count = {}
-        # #initialize '_obj' attribute last. Trust me on this!
-        # self._obj = target_object
+        # self._obj = target_object #this part was originally included, but I edited it out and put in the code below instead. I think it made sure that obj was initialized on Proxy itself and not just the instances, but I don't know for certain. 
         object.__setattr__(self, '_call_count', {})
         object.__setattr__(self, '_obj', target_object)
 
 
 
     # WRITE CODE HERE
-    def __getattribute__(self,name):
-        # if '_obj' == name:
-        #     pass
-        # if name in self._count:
-        #     self._call_count[name] += 1
-        #     print(self._call_count)
-        # else:
-        #     self._call_count[name] = 1
+
+    def __getattr__(self, name):  # changed this to __getattr__ instead of __getattribute__ not entirely sure if that was what I was supposed to do. Docs say "Called when the default attribute access fails with an AttributeError (either __getattribute__() raises an AttributeError because name is not an instance attribute or an attribute in the class tree for self; or __get__() of a name property raises AttributeError)." not sure what that means though
+
+        if name in self._call_count:
+            self._call_count[name] = self._call_count[name] + 1
+        else: 
+            self._call_count[name] = 1
         return getattr(object.__getattribute__(self, "_obj"), name)
     def __setattr__(self, name, value):
+        if name in self._call_count:
+            self._call_count[name] = self._call_count[name] + 1
+        else:
+            self._call_count[name] = 1  #originally forgot to add the name to my list for setattr. 
         setattr(object.__getattribute__(self, "_obj"), name, value)
     def messages(self):
-        return self._call_count.keys
+        return [*self._call_count] #astrisk unpacks and returns just the key values to a list. 
+    def was_called(self, name):
+        return name in self._call_count  #returns true if called, false otherwise. 
+    def number_of_times_called(self, name):
+        if name in self._call_count:
+            return self._call_count[name]
+        else:
+            return 0
 
 
 # The proxy object should pass the following Koan:
@@ -64,55 +73,53 @@ class AboutProxyObjectProject(Koan):
         self.assertTrue(tv.is_on())
 
     def test_proxy_records_messages_sent_to_tv(self):
-        pass
-        # tv = Proxy(Television())
+        tv = Proxy(Television())
 
-        # tv.power()
-        # tv.channel = 10
+        tv.power()
+        tv.channel = 10
 
-        # self.assertEqual(['power', 'channel'], tv.messages())
+        self.assertEqual(['power', 'channel'], tv.messages())
 
     def test_proxy_handles_invalid_messages(self):
-        pass
-        # tv = Proxy(Television())
+        tv = Proxy(Television())
 
-        # with self.assertRaises(AttributeError):
-        #     tv.no_such_method()
+        with self.assertRaises(AttributeError):
+            tv.no_such_method()
 
 
     def test_proxy_reports_methods_have_been_called(self):
         tv = Proxy(Television())
-        pass
 
-        # tv.power()
-        # tv.power()
+        tv.power()
+        tv.power()
 
-        # self.assertTrue(tv.was_called('power'))
-        # self.assertFalse(tv.was_called('channel'))
+        self.assertTrue(tv.was_called('power'))
+        self.assertFalse(tv.was_called('channel'))
 
     def test_proxy_counts_method_calls(self):
         tv = Proxy(Television())
-        pass
-        # tv.power()
-        # tv.channel = 48
-        # tv.power()
 
-        # self.assertEqual(2, tv.number_of_times_called('power'))
-        # self.assertEqual(1, tv.number_of_times_called('channel'))
-        # self.assertEqual(0, tv.number_of_times_called('is_on'))
+        tv.power()
+        tv.channel = 48
+        tv.power()
+
+        self.assertEqual(2, tv.number_of_times_called('power'))
+        self.assertEqual(1, tv.number_of_times_called('channel'))
+        self.assertEqual(0, tv.number_of_times_called('is_on'))
 
     def test_proxy_can_record_more_than_just_tv_objects(self):
-        # proxy = Proxy("Py Ohio 2010")
+        proxy = Proxy("Py Ohio 2010")
 
-        # result = proxy.upper()
+        result = proxy.upper()
 
-        # self.assertEqual("PY OHIO 2010", result)
+        self.assertEqual("PY OHIO 2010", result)
 
-        # result = proxy.split()
+        result = proxy.split()
 
-        # self.assertEqual(["Py", "Ohio", "2010"], result)
-        # self.assertEqual(['upper', 'split'], proxy.messages())
-        pass
+        self.assertEqual(["Py", "Ohio", "2010"], result)
+        self.assertEqual(['upper', 'split'], proxy.messages())
+
+        #proxy is tracking built in expressions here. 
 
 # ====================================================================
 # The following code is to support the testing of the Proxy class.  No
